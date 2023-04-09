5360d72f745c7aa6bb44d12a39d9bb0c2a78c38a
Bao Thanh <57084519+vexagonverp@users.noreply.github.com>
2023-04-09 21:42:25 +0700

    Esline ignore .d.ts

--- src/types/preload.d.ts
+++ src/types/preload.d.ts
@@ -2,8 +2,11 @@ declare global {
   interface Window {
     ipcChannel: {
       /* ELECTRON IPC TYPES */
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       send(channel: string, ...args: any[]): void;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       receive(channel: string, callback: (...args: any[]) => void): void;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       sendAndReceive: (channel: string, ...args: any[]) => Promise<any> | undefined;
     };
     ipcStorage: {
