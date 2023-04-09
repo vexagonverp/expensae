88ca22e28c8630b50092c15030f266fa9edd23d8
Bao Thanh <57084519+vexagonverp@users.noreply.github.com>
2023-04-09 21:36:38 +0700

    IPC Preload

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,2 +1,29 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from 'electron';
+import ipcMsg from '../shared/ipcMsg';
+
+contextBridge.exposeInMainWorld('ipcChannel', {
+  // From render to main.
+  send: (channel: string, ...args: unknown[]) => {
+    const validChannels = Object.values(ipcMsg.RendererToMain);
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, ...args);
+    }
+  },
+  // From main to render.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  receive: (channel: string, callback: (...args0: any) => void) => {
+    const validChannels = Object.values(ipcMsg.MainToRenderer);
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => callback(...args));
+    }
+  },
+  // From render to main and back again.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  sendAndReceive: (channel: string, ...args: any[]): Promise<any> | undefined => {
+    const validChannels = Object.values(ipcMsg.RendererMainRenderer);
+    if (validChannels.includes(channel)) {
+      return ipcRenderer.invoke(channel, args);
+    }
+    return undefined;
+  }
+});
---
+++ src/shared/ipcMsg.ts
@@ -0,0 +1,7 @@
+const ipcMsg = {
+  RendererToMain: {},
+  MainToRenderer: {},
+  RendererMainRenderer: {}
+};
+
+export default ipcMsg;
