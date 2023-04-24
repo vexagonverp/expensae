bfaf8fbd903720dc24f02f0e78e49144b4dea56a
thanh.tranbao <57084519+vexagonverp@users.noreply.github.com>
2023-04-24 11:27:17 +0700

    Submit tweak

--- src/renderer/route/SubmitFormRoute.tsx
+++ src/renderer/route/SubmitFormRoute.tsx
@@ -1,26 +1,48 @@
 import { Button, Form, Input, Space } from 'antd';
+import { useState } from 'react';
 import ipcMsg from '../../shared/ipcMsg';
 import { SheetPayload } from '../interfaces';
 
+const enum ItemStatusState {
+  BLANK = '',
+  SUCCESS = 'success',
+  VALIDATING = 'validating',
+  ERROR = 'error'
+}
 const SubmitFormRoute = () => {
   const [form] = Form.useForm();
+  const [itemStatus, setItemStatus] = useState(ItemStatusState.BLANK);
   const onFinish = (payload: SheetPayload) => {
+    setItemStatus(ItemStatusState.VALIDATING);
     window.ipcChannel.sendAndReceive(ipcMsg.RendererMainRenderer.SHEET_ID, payload);
   };
   const { Item } = Form;
+  const { Compact } = Space;
   return (
-    <Space.Compact style={{ width: '100%' }}>
+    <Compact style={{ width: '100%' }}>
       <Form form={form} onFinish={onFinish}>
-        <Item name="sheetId" label="Google sheet's Id" rules={[{ required: true }]}>
+        <Item
+          name="sheetId"
+          label="Google sheet's Id"
+          rules={[{ required: true }]}
+          hasFeedback
+          validateStatus={itemStatus}
+        >
           <Input />
         </Item>
-        <Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
+        <Item shouldUpdate>
+          {({ getFieldsValue }) => {
+            const { sheetId } = getFieldsValue();
+            const formIsComplete = sheetId;
+            return (
+              <Button type="primary" htmlType="submit" disabled={!formIsComplete}>
+                Submit
+              </Button>
+            );
+          }}
         </Item>
       </Form>
-    </Space.Compact>
+    </Compact>
   );
 };
 
